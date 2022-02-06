31b41b64e2c832251e1bed53701e2f4c1afaac90
Rodion Yeroshek <yeroshek@gmail.com>
2022-02-06 23:04:15 +0200

    Add Blender model to the scene

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls.js';
 import { FlyControls } from 'three/examples/jsm/controls/FlyControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 /**
  * Base
@@ -12,8 +13,8 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color( 0xffffff );
-scene.fog = new THREE.Fog( 0xffffff, 0, 2000 );
+// scene.background = new THREE.Color( 0xffffff );
+// scene.fog = new THREE.Fog( 0xffffff, 0, 2000 );
 
 /**
  * Sizes
@@ -43,9 +44,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 1
+camera.position.x = -3
+camera.position.y = 3
+camera.position.z = 10
 scene.add(camera)
 
 // Controls
@@ -56,21 +57,31 @@ controls.dragToLook = true;
 /**
  * Cube
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshLambertMaterial({ color: 0xff0000 })
-)
-scene.add(cube)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshLambertMaterial({ color: 0xff0000 })
+// )
+// scene.add(cube);
 
-for (let i = 0; i < 500; i++) {
+const torusLoader = new GLTFLoader();
+
+torusLoader.load('./torus.gltf', (torus) => {
+    scene.add(torus.scene);
+})
+
+const particles = [];
+
+for (let i = 0; i < 5000; i++) {
     let particle = new THREE.Mesh(
         new THREE.BoxGeometry(0.03, 0.03, 0.03),
-        new THREE.MeshBasicMaterial({ color: 0xff0000 })
+        new THREE.MeshBasicMaterial({ color: 0xffffff })
     )
 
-    particle.position.x = Math.random() * 20 - 10;
-    particle.position.y = Math.random() * 20 - 10;
-    particle.position.z = Math.random() * 20 - 10;
+    particle.position.x = Math.random() * 40 - 20;
+    particle.position.y = Math.random() * 40 - 20;
+    particle.position.z = Math.random() * 40 - 20;
+
+    particles.push(particle);
 
     scene.add(particle);
 }
@@ -82,11 +93,10 @@ for (let i = 0; i < 500; i++) {
 const ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffff00, 1);
-directionalLight.castShadow = true;
-directionalLight.position.z = 5;
-directionalLight.position.x = 2;
-directionalLight.position.y = 3;
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.z = 100;
+directionalLight.position.x = 0;
+directionalLight.position.y = 0;
 scene.add(directionalLight);
 
 
@@ -115,11 +125,18 @@ const tick = () =>
     // Update controls
     controls.update(deltaTime)
 
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].position.z += 0.001;
+    }
+
+    directionalLight.position.x += 1;
+    directionalLight.position.y += 1;
+
     // Render
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    window.requestAnimationFrame(tick);
 }
 
 tick()
